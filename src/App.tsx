6ede36df13b6dcf8ceaa571c1e473bd04fdd0970
Desimir Dimitrov <desimir.dimitrov@orbisagency.com>
2023-10-16 23:37:30 +0300

    Working version

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,143 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { Key } from "chessground/types";
+import Chessground from "@react-chess/chessground";
+import useChess from "./hooks/useChess";
+import queen from "./assets/images/wQ.svg";
+import rook from "./assets/images/wR.svg";
+import bishop from "./assets/images/wB.svg";
+import knight from "./assets/images/wN.svg";
+import "./App.css";
+import useAgora from "./hooks/useAgora";
+
+declare let window: any;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {
+    chess,
+    turnColor,
+    undoMove,
+    lastMove,
+    fen,
+    promotion,
+    calcMovable,
+    promotionModal,
+    movesLog,
+  } = useChess();
+  const { sendMsgInChannel } = useAgora();
 
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+  const [innerHeight, setInnerHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    updateDimensions();
+    window.addEventListener("resize", updateDimensions);
+
+    return () => window.removeEventListener("resize", updateDimensions);
+  }, []);
+
+  const updateDimensions = () => {
+    setInnerWidth(window.innerWidth);
+    setInnerHeight(window.innerHeight);
+  };
+
+  let config = {
+    fen: fen,
+    coordinates: true,
+    turnColor: turnColor(),
+    addPieceZIndex: true,
+    autoCastle: true,
+    lastMove: lastMove,
+    highlight: {
+      lastMove: true,
+      check: true,
+    },
+    // animation: {
+    //   enabled: true,
+    //   duration: 5000,
+    // },
+    selectable: {
+      enabled: true,
+    },
+    movable: calcMovable(),
+    events: {
+      select: (key: Key) => {
+        // setSelectedChesspiece(key);
+        // console.log(new Map(key, chess.moves({square: key})));
+      },
+    },
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app" id="chessboard">
+      <div className="side-section">
+        <div>
+          {movesLog?.map((move: String) => (
+            <>{move}, </>
+          ))}
+        </div>
+        {promotionModal && (
+          <div className="piece-promotion-prompt">
+            Promote To?
+            <div className="roles">
+              <span role="presentation" onClick={() => promotion("q")}>
+                <img src={queen} alt="" style={{ width: 50 }} />
+              </span>
+              <span role="presentation" onClick={() => promotion("r")}>
+                <img src={rook} alt="" style={{ width: 50 }} />
+              </span>
+              <span role="presentation" onClick={() => promotion("b")}>
+                <img src={bishop} alt="" style={{ width: 50 }} />
+              </span>
+              <span role="presentation" onClick={() => promotion("n")}>
+                <img src={knight} alt="" style={{ width: 50 }} />
+              </span>
+            </div>
+          </div>
+        )}
+        {chess.fen() !==
+          "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" && (
+          <div
+            className="undo-btn"
+            onClick={() => {
+              undoMove();
+            }}
+          >
+            Undo
+          </div>
+        )}
+        <div>
+          <div
+            id="chat-section"
+            className="chat-section"
+            style={{ padding: 10, border: "black" }}
+          ></div>
+          <div style={{ display: "flex" }}>
+            <input type="text" id="chatbox" />
+            <button
+              onClick={() => {
+                sendMsgInChannel(
+                  window.document.getElementById("chatbox").value
+                );
+              }}
+              style={{ padding: 10 }}
+            >
+              Send
+            </button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="main-chessboard">
+        <Chessground
+          width={
+            innerWidth < innerHeight ? innerWidth * 0.5 : innerHeight * 0.5
+          }
+          height={
+            innerWidth < innerHeight ? innerWidth * 0.5 : innerHeight * 0.5
+          }
+          config={{ ...config }}
+        />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
